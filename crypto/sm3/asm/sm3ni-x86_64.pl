feaf1952f23fad3eefe321055a27cc846aeb32b8
Elizarova, Alina <alina.elizarova@intel.com>
2024-07-15 06:56:44 -0700

    cleaned-up asm

--- crypto/sm3/asm/sm3ni-x86_64.pl
+++ crypto/sm3/asm/sm3ni-x86_64.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/env perl
-# Copyright 2024-2024 The OpenSSL Project Authors. All Rights Reserved.
+# Copyright 2024 The OpenSSL Project Authors. All Rights Reserved.
 #
 # Licensed under the Apache License 2.0 (the "License").  You may not use
 # this file except in compliance with the License.  You can obtain a copy
@@ -22,20 +22,20 @@ $dir=$1;
 ( $xlate="${dir}../../perlasm/x86_64-xlate.pl" and -f $xlate) or
 die "can't locate x86_64-xlate.pl";
 
-# Need a meaningful check here instead of $avx
-# if (`$ENV{CC} -Wa,-v -c -o /dev/null -x assembler /dev/null 2>&1`
-# 		=~ /GNU assembler version ([2-9]\.[0-9]+)/) {
-# 	$avx = ($1>=2.19) + ($1>=2.22);
-# }
+# Check Intel(R) SM3 instructions support
+if (`$ENV{CC} -Wa,-v -c -o /dev/null -x assembler /dev/null 2>&1`
+		=~ /GNU assembler version ([2-9]\.[0-9]+)/) {
+	$avx2_sm3_ni = ($1>=2.40);
+}
 
-# if (!$avx && $win64 && ($flavour =~ /nasm/ || $ENV{ASM} =~ /nasm/) &&
-# 	   `nasm -v 2>&1` =~ /NASM version ([2-9]\.[0-9]+)/) {
-# 	$avx = ($1>=2.09) + ($1>=2.10);
-# }
+if (!$avx2_sm3_ni && $win64 && ($flavour =~ /nasm/ || $ENV{ASM} =~ /nasm/) &&
+	   `nasm -v 2>&1` =~ /NASM version ([2-9]\.[0-9]+)/) {
+	$avx2_sm3_ni = ($1>=2.16); # support added at NASM 2.16.02
+}
 
-# if (!$avx && `$ENV{CC} -v 2>&1` =~ /((?:clang|LLVM) version|.*based on LLVM) ([0-9]+\.[0-9]+)/) {
-# 	$avx = ($2>=3.0) + ($2>3.0);
-# }
+if (!$avx2_sm3_ni && `$ENV{CC} -v 2>&1` =~ /((?:clang|LLVM) version|.*based on LLVM) ([0-9]+\.[0-9]+)/) {
+	$avx2_sm3_ni = ($2>=17.0); # support added at LLVM 17.0.1
+}
 
 open OUT,"| \"$^X\" \"$xlate\" $flavour \"$output\""
     or die "can't call $xlate: $!";
@@ -44,13 +44,11 @@ open OUT,"| \"$^X\" \"$xlate\" $flavour \"$output\""
 @_3args=$win64?	("%rcx","%rdx","%r8") :	# Win64 order
         ("%rdi","%rsi","%rdx");	# Unix order
 
-#if ($avx>0) { #<<<
+if ($avx2_sm3_ni>0) {
 # Create 4 x 32-bit new words of message schedule W[] using SM3-NI ISA
 sub sm3msg {
 my ($W03_00, $W07_04, $W11_08, $W15_12, $W19_16, $T1,$T2) = @_;
 my $T3 = $W19_16;
-# Q: is it correct to write `3*4`?
-# Q: parameters order?
 $code.=<<___;
     vpalignr        \$12, $W07_04, $W11_08, $T3
     vpsrldq         \$4, $W15_12, $T1
@@ -65,7 +63,6 @@ ___
 #   - consumes 4 words of message schedule W[]
 #   - updates SM3 state registers: ABEF and CDGH
 sub sm3rounds4 {
-# Q: the last parameter of vsm3rnds2 syntax?
 my ($ABEF, $CDGH, $W03_00, $W07_04, $T1,$R)=@_;
 $code.=<<___;
     vpunpcklqdq     $W07_04, $W03_00, $T1
@@ -75,33 +72,31 @@ $code.=<<___;
 ___
 }
 
+$code.= ".data\n";
+{
+# input arguments aliases
+my ($ctx,$p,$num) = @_3args;
+
+$code.=<<___;
+.align 16
+.SHUFF_MASK:
+    .byte 3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12
+
+.text
+
 # void ossl_hwsm3_block_data_order(SM3_CTX *c, const void *p, size_t num)
 #
 # input: $ctx SM3 context
 #        $p  pointer to the data
 #        $num number of blocks
 #
-my ($ctx,$p,$num) = @_3args;
 
-# Q: changing order in mov for AT&T?
-# Q: syntax vmovdqu         16($ctx), %xmm7 ?
-# Q: vpshufd parameters order?
-# Q: vpxor + q? for xmm registers?
-# Q: shuffle mask?
-$code.= ".text\n";
-
-# {
-$code.=<<___;
-.align 16
-.SHUFF_MASK:
-    #.byte 0x03, 0x02, 0x01, 0x00, 0x07, 0x06, 0x05, 0x04, 0x11, 0x10, 0x09, 0x08, 0x15, 0x14, 0x13, 0x12
-    .byte 3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12
 .globl	ossl_hwsm3_block_data_order
 .type	ossl_hwsm3_block_data_order,\@function,3
 .align	32
 ossl_hwsm3_block_data_order:
-    # note: wrong instruction!!! should be movsxd
-    movsxl   %edx, %rdx
+    or $num, $num
+    je .done_hash
 
     vmovdqu         ($ctx), %xmm6
     vmovdqu         16($ctx), %xmm7
@@ -138,146 +133,126 @@ ossl_hwsm3_block_data_order:
 ___
     sm3msg("%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm2", "%xmm3", "%xmm1", 0);
-    # SM3MSG          %xmm2, %xmm3, %xmm4, %xmm5, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W19 W18 W17 W16
-    # SM3ROUNDS4      %xmm6, %xmm7, %xmm2, %xmm3, %xmm1, $0
 $code.=<<___;
     vmovdqa         %xmm8, %xmm2
 ___
     sm3msg("%xmm3", "%xmm4", "%xmm5", "%xmm2", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm3", "%xmm4", "%xmm1", 4);
-    # SM3MSG          %xmm3, %xmm4, %xmm5, %xmm2, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W23 W22 W21 W20
-    # SM3ROUNDS4      %xmm6, %xmm7, %xmm3, %xmm4, %xmm1, $4
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm3
 ___
     sm3msg("%xmm4", "%xmm5", "%xmm2", "%xmm3", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm4", "%xmm5", "%xmm1", 8);
-    # SM3MSG          %xmm4, %xmm5, %xmm2, %xmm3, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W27 W26 W25 W24
-    # SM3ROUNDS4      %xmm6, %xmm7, %xmm4, %xmm5, %xmm1, $8
+
 $code.=<<___;
     vmovdqa         %xmm8, %xmm4
 ___
     sm3msg("%xmm5", "%xmm2", "%xmm3", "%xmm4", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm5", "%xmm2", "%xmm1", 12);
-    # SM3MSG          %xmm5, %xmm2, %xmm3, %xmm4, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W31 W30 W29 W28
-    # SM3ROUNDS4      %xmm6, %xmm7, %xmm5, %xmm2, %xmm1, $12
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm5
 ___
     sm3msg("%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm2", "%xmm3", "%xmm1", 16);
-    # SM3MSG          %xmm2, %xmm3, %xmm4, %xmm5, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W35 W34 W33 W32
-    # SM3ROUNDS4      %xmm6, %xmm7, %xmm2, %xmm3, %xmm1, $16
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm2
 ___
     sm3msg("%xmm3", "%xmm4", "%xmm5", "%xmm2", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm3", "%xmm4", "%xmm1", 20);
-    # SM3MSG          %xmm3, %xmm4, %xmm5, %xmm2, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W39 W38 W37 W36
-    # SM3ROUNDS4      %xmm6, %xmm7, %xmm3, %xmm4, %xmm1, $20
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm3
 ___
     sm3msg("%xmm4", "%xmm5", "%xmm2", "%xmm3", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm4", "%xmm5", "%xmm1", 24);
-    # # SM3MSG          %xmm4, %xmm5, %xmm2, %xmm3, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W43 W42 W41 W40
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm4, %xmm5, %xmm1, $24
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm4
 ___
     sm3msg("%xmm5", "%xmm2", "%xmm3", "%xmm4", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm5", "%xmm2", "%xmm1", 28);
-    # # SM3MSG          %xmm5, %xmm2, %xmm3, %xmm4, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W47 W46 W45 W44
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm5, %xmm2, %xmm1, $28
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm5
 ___
     sm3msg("%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm2", "%xmm3", "%xmm1", 32);
-    # # SM3MSG          %xmm2, %xmm3, %xmm4, %xmm5, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W51 W50 W49 W48
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm2, %xmm3, %xmm1, $32
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm2
 ___
     sm3msg("%xmm3", "%xmm4", "%xmm5", "%xmm2", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm3", "%xmm4", "%xmm1", 36);
-    # # SM3MSG          %xmm3, %xmm4, %xmm5, %xmm2, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W55 W54 W53 W52
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm3, %xmm4, %xmm1, $36
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm3
 ___
     sm3msg("%xmm4", "%xmm5", "%xmm2", "%xmm3", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm4", "%xmm5", "%xmm1", 40);
-    # # SM3MSG          %xmm4, %xmm5, %xmm2, %xmm3, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W59 W58 W57 W56
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm4, %xmm5, %xmm1, $40
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm4
 ___
     sm3msg("%xmm5", "%xmm2", "%xmm3", "%xmm4", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm5", "%xmm2", "%xmm1", 44);
-    # # SM3MSG          %xmm5, %xmm2, %xmm3, %xmm4, %xmm8, %xmm9, %xmm1        # ?? xmm8 = W63 W62 W61 W60
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm5, %xmm2, %xmm1, %44
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm5
 ___
     sm3msg("%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm8", "%xmm9", "%xmm1");
     sm3rounds4("%xmm6", "%xmm7", "%xmm2", "%xmm3", "%xmm1", 48);
-    # # SM3MSG          %xmm2, %xmm3, %xmm4, %xmm5, %xmm8, %xmm9, %xmm1       # ?? xmm8 = W67 W66 W65 W64
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm2, %xmm3, %xmm1, $48
 
 $code.=<<___;
     vmovdqa         %xmm8, %xmm2
 ___
 
     sm3rounds4("%xmm6", "%xmm7", "%xmm3", "%xmm4", "%xmm1", 52);
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm3, %xmm4, %xmm1, $52
 
     sm3rounds4("%xmm6", "%xmm7", "%xmm4", "%xmm5", "%xmm1", 56);
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm4, %xmm5, %xmm1, $56
 
-   sm3rounds4("%xmm6", "%xmm7", "%xmm5", "%xmm2", "%xmm1", 60);
-    # # SM3ROUNDS4      %xmm6, %xmm7, %xmm5, %xmm2, %xmm1, $60
+    sm3rounds4("%xmm6", "%xmm7", "%xmm5", "%xmm2", "%xmm1", 60);
 
 $code.=<<___;
     # update hash value
     vpxor           %xmm10, %xmm6, %xmm6
     vpxor           %xmm11, %xmm7, %xmm7
-    addq             \$64,($p)
+    addq             \$64, $p
     dec             $num
     jnz             .block_loop
 
     # store the hash value back in memory
     vpslld          \$9, %xmm7, %xmm2
     vpsrld          \$23, %xmm7, %xmm3
-    vpxor           %xmm3, %xmm2, %xmm1        # xmm1 = xmm2 ^ xmm3 = ROL32(CDGH, 9)
+    vpxor           %xmm3, %xmm2, %xmm1
     vpslld          \$19, %xmm7, %xmm4
     vpsrld          \$13, %xmm7, %xmm5
-    vpxor           %xmm5, %xmm4, %xmm0         # xmm0 = xmm2 ^ xmm3 = ROL32(CDGH, 19)
-    vpblendd        \$0x3, %xmm0, %xmm1, %xmm7   # xmm7 = ROL32(C, 9) ROL32(D, 9) ROL32(G, 19) ROL32(H, 19)
-    vpshufd         \$0x1B, %xmm6, %xmm0,        # xmm0 = F E B A
-    vpshufd         \$0x1B, %xmm7, %xmm1         # xmm1 = H G D C
+    vpxor           %xmm5, %xmm4, %xmm0
+    vpblendd        \$0x3, %xmm0, %xmm1, %xmm7
+    vpshufd         \$0x1B, %xmm6, %xmm0
+    vpshufd         \$0x1B, %xmm7, %xmm1
 
-    vpunpcklqdq     %xmm1, %xmm0, %xmm6        # xmm6 = D C B A
-    vpunpckhqdq     %xmm1, %xmm0, %xmm7           # xmm7 = H G F E
+    vpunpcklqdq     %xmm1, %xmm0, %xmm6
+    vpunpckhqdq     %xmm1, %xmm0, %xmm7
 
-    # order?
     vmovdqu         %xmm6, ($ctx)
     vmovdqu         %xmm7, 16($ctx)
-
+.done_hash:
    ret
 ___
-#}
-
+}
+} else { # fallback
+$code .= <<___;
+.text
+.type ossl_hwsm3_block_data_order,\@abi-omnipotent
+ossl_hwsm3_block_data_order:
+    .byte   0x0f,0x0b    # ud2
+    ret
+.size   ossl_hwsm3_block_data_order, .-ossl_hwsm3_block_data_order
+___
+} # avx2_sm3_ni
 
 $code =~ s/\`([^\`]*)\`/eval $1/gem;
 print $code;
